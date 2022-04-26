9cae61400b637811e3e2d098ccd2b5d6023f6b34
Anaroco <roblescortes@msn.com>
2022-04-26 12:30:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# RepositorioBacked
+Proyecto creado para la Hackaton F5, consistente en crear una tienda de moviles de segunda mano para poderlos reutilizar.
+
+## Stack utilizado
+Java
+Spring boot
+Html
+Css
+Vue
+
+
---
+++ src/test/java/com/marketplace/NewPhone/NewPhoneTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.NewPhone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPhoneTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
